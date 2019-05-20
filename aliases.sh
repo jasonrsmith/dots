f594ff501adf055fa9dd56c6eb37b21cecf2e0b4
Jason Smith <eastcoastj@gmail.com>
2019-05-19 18:52:43 -0700

    some aliases

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+alias godot=/Applications/Godot.app/Contents/MacOS/Godot
+alias gut='godot --debug-collisions --path $PWD -d -s addons/gut/gut_cmdln.gd -ginclude_subdirs -gdir="res://" -gsuffix=".test.gd" -gprefix= -gexit'
+
