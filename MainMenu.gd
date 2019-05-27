6eef7d359b03f0c8e650bc6c9cecc7875bb8e0b1
Jason Smith <eastcoastj@gmail.com>
2019-05-27 15:24:55 -0700

    code cleanup

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,30 +1,23 @@
 extends MarginContainer
 
-var menuItems
-var selectedItem
+onready var cursor: Label = find_node("Cursor")
+onready var menu_items: VBoxContainer = find_node("MenuItems")
+
+var _menu_item_list
+var _currently_selected_item
 
 signal menu_item_selected
 
-func repositionCursor():
-	find_node("Cursor").margin_top = self.menuItems[selectedItem].margin_top + 10
-
-func cursorUp():
-	self.selectedItem = self.menuItems.size() - 1 if self.selectedItem == 0 else self.selectedItem - 1	
-
-func cursorDown():
-	self.selectedItem = (self.selectedItem + 1) % self.menuItems.size()
-
-func acceptSelection():
-	self.emit_signal('menu_item_selected', menuItems[selectedItem].name)
-
-func _process(delta):
+func _process(delta: float) -> void:
 	self.repositionCursor()
 
-func _ready():
-	self.menuItems = find_node("Menu").get_children()
-	self.selectedItem = 0
 
-func _unhandled_input(ev):
+func _ready() -> void:
+	self._menu_item_list = menu_items.get_children()
+	self._currently_selected_item = 0
+
+
+func _unhandled_input(ev) -> void:
 	if !visible:
 		return
 	if Input.is_action_just_pressed("ui_up"):
@@ -35,3 +28,20 @@ func _unhandled_input(ev):
 		self.acceptSelection()
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+
+
+func repositionCursor() -> void:
+	cursor.margin_top = self._menu_item_list[_currently_selected_item].margin_top + 10
+
+
+func cursorUp() -> void:
+	self._currently_selected_item = self._menu_item_list.size() - 1 if self._currently_selected_item == 0 else self._currently_selected_item - 1	
+
+
+func cursorDown() -> void:
+	self._currently_selected_item = (self._currently_selected_item + 1) % self._menu_item_list.size()
+
+
+func acceptSelection() -> void:
+	self.emit_signal('menu_item_selected', _menu_item_list[_currently_selected_item].name)
+
