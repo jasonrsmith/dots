97f0f0561d677712cb8d67c55ad115af1ff9adb5
Jason Smith <eastcoastj@gmail.com>
2019-05-27 22:13:35 -0700

    game over dialog

--- PlayScreen.gd
+++ PlayScreen.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
+onready var leave_game_dialog: Popup = $LeaveGameDialog
+onready var lose_game_dialog: Popup = $LoseGameDialog
+onready var player_board: Board = $PlayerBoard
+onready var opponent_board: Board = $opponentBoard
+
 signal leave_screen
 
-func leaveScreen():
-	print("leave_screen")
-	emit_signal("leave_screen")
-
 func _ready():
-	$LeaveGameDialog.connect("confirmed", self, "leaveScreen")
+	leave_game_dialog.connect("confirmed", self, "leave_screen")
+	player_board.connect('board_full', self, '_on_PlayerBoard_board_full')
+
 
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("ui_cancel") && $LeaveGameDialog.visible == false:
-		$LeaveGameDialog.popup_centered()
+	if Input.is_action_just_pressed("ui_cancel") && leave_game_dialog.visible == false:
+		leave_game_dialog.popup_centered()
 		get_tree().set_input_as_handled()
+
+
+func leave_screen() -> void:
+	emit_signal("leave_screen")
+
+
+func _on_PlayerBoard_board_full() -> void:
+	lose_game_dialog.popup_centered()
